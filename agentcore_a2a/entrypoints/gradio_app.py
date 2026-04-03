aa1f942a66c7279f2cbfd448d28e6ef51ab9d0f6
Aniruddha Kate <aniruddhaka@cybage.com>
2026-04-03 14:07:43 +0530

    Fixed agent invocation error

--- agentcore_a2a/entrypoints/gradio_app.py
+++ agentcore_a2a/entrypoints/gradio_app.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import sys
+
+if __package__ in (None, ""):
+    sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+
 from agentcore_a2a.ui.gradio_chat_app import GradioChatApplication
 
 demo = GradioChatApplication().create_demo()
